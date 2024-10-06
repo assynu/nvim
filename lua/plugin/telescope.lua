e91d6b2f16c6a81223c2da4a730e8a39d3fbaae7
Dawid Bartczak <66019090+assynu@users.noreply.github.com>
2024-10-07 00:10:39 +0100

    tweak: add fugitive and telescope extensions

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -5,10 +5,33 @@ return {
         config = function()
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+
+            require("telescope").setup({
+                pickers = {
+                    find_files = {
+                        hidden = true,
+                        find_command = {
+                            "rg",
+                            "--files",
+                            "--glob",
+                            "!{.git/*,.svelte-kit/*,target/*,node_modules/*}",
+                            "--path-separator",
+                            "/",
+                        },
+                    },
+                },
+            })
         end
     },
     {
         "nvim-telescope/telescope-file-browser.nvim",
-        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+        config = function()
+            vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>") 
+        end
+    },
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
     }
 }
