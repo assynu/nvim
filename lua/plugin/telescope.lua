dba16e57a112dc479c04e0090b1d6a5f8b9c35b2
Dawid Bartczak <66019090+assynu@users.noreply.github.com>
2024-10-07 09:44:34 +0100

    feat: add telescope grep

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -1,13 +1,17 @@
 return {
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            {
+                "nvim-telescope/telescope-live-grep-args.nvim" ,
+                version = "^1.0.0",
+            }
+        },
         config = function()
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-            vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find git files' })
+            local telescope = require('telescope')
 
-            require("telescope").setup({
+            telescope.setup({
                 pickers = {
                     find_files = {
                         hidden = true,
@@ -22,13 +26,21 @@ return {
                     },
                 },
             })
+
+            telescope.load_extension("live_grep_args")
+
+            local builtin = require('telescope.builtin')
+            
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
+            vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
         end
     },
     {
         "nvim-telescope/telescope-file-browser.nvim",
         dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
         config = function()
-            vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>") 
+            vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>")
         end
     },
     {
