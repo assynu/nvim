f9895343f012023eb4fd11ea3143a4479b115682
Dawid Bartczak <66019090+assynu@users.noreply.github.com>
2024-10-07 08:26:46 +0100

    tweak: telescope mapping

---
+++ lua/plugin/mason.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require('mason').setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require('mason-lspconfig').setup({
+                ensure_installed = { 'lua_ls', 'csharp_ls', 'ts_ls', 'jsonls' },
+                handlers = {
+                    function(server_name)
+                        require('lspconfig')[server_name].setup({})
+                    end,
+                }
+            })
+        end
+    }
+}
