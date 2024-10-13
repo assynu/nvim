6c10995e66e77cdf14ab1f1c1fcd871db39cfe25
Dawid <66019090+assynu@users.noreply.github.com>
2024-10-13 22:08:28 +0200

    feat: buffer navigation keybinds

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>p", vim.cmd.bprevious)
+vim.keymap.set("n", "<leader>n", vim.cmd.bnext)
+vim.keymap.set("n", "<leader>d", vim.cmd.bdelete)
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -20,7 +20,37 @@ return {
                 },
                 handlers = {
                     function(server_name)
-                        require('lspconfig')[server_name].setup({})
+                        if server_name == 'lua_ls' then
+                            require('lspconfig')[server_name].setup({
+                                on_init = function(client)
+                                    if client.workspace_folders then
+                                        local path = client.workspace_folders[1].name
+                                        if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+                                            return
+                                        end
+                                    end
+
+                                    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                                        runtime = {
+                                            version = "Lua 5.4"
+                                        },
+                                        workspace = {
+                                            checkThirdParty = false,
+                                            library = {
+                                                vim.env.VIMRUNTIME
+                                                "C:/Users/dawid/Documents/LuaExtension/fivem-lls-addon/library",
+                                                "/mnt/c/Users/dawid/Documents/LuaExtension/fivem-lls-addon/library"
+                                            }
+                                        }
+                                    })
+                                end,
+                                settings = {
+                                    Lua = {}
+                                }
+                            })
+                        else
+                            require('lspconfig')[server_name].setup({})
+                        end
                     end,
                 }
             })
