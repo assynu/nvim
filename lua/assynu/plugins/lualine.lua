99fd53bd0ca0ad2298995fdf5639b6b3f98e0ed3
Dawid Bartczak <66019090+assynu@users.noreply.github.com>
2024-12-07 21:37:54 +0100

    feat: add lualine

--- lua/assynu/plugins/lualine.lua
+++ lua/assynu/plugins/lualine.lua
@@ -2,8 +2,10 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local customTheme = require("lualine.themes.nightfly")
+
         require('lualine').setup {
---            options = { theme = 'gruvbox' }
+            options = { theme = customTheme }
         }
     end
 }
