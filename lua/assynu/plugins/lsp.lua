029ae4918e0202d8a8bb814ec6dcf7f43f424d46
Dawid <66019090+assynu@users.noreply.github.com>
2024-12-13 10:45:40 +0100

    feat: c# lsp

--- lua/assynu/plugins/lsp.lua
+++ lua/assynu/plugins/lsp.lua
@@ -28,7 +28,8 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "ts_ls",
-                "clangd"
+                "clangd",
+                "csharp_ls"
             },
             handlers = {
                 function(server_name) -- default handler (optional)
