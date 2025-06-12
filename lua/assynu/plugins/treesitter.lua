bdc91abd849ad89bbc2ae8e2cece6e725e0612a1
Dawid <66019090+assynu@users.noreply.github.com>
2025-06-12 17:18:40 +0200

    fix: treesitter highlighting

--- lua/assynu/plugins/treesitter.lua
+++ lua/assynu/plugins/treesitter.lua
@@ -1,13 +1,18 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "lua", "vim", "vimdoc", "query", "css", "typescript", "javascript", "html", "zig", "go" },
-          sync_install = false,
-          indent = { enable = true },
-      })
-  end
- }
+        configs.setup({
+            ensure_installed = { "lua", "vim", "vimdoc", "query", "css", "typescript", "javascript", "html", "zig", "go" },
+            sync_install = false,
+            auto_install = true,
+
+            indent = { enable = true },
+            highlight = {
+                enable = true
+            }
+        })
+    end
+}
