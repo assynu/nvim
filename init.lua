860cbd0da01217cf00f8c519b90b4f422abeade3
Dawid <66019090+assynu@users.noreply.github.com>
2025-06-17 00:06:44 +0200

    fix: lua_ls settings issue

--- init.lua
+++ init.lua
@@ -25,7 +25,7 @@ vim.o.signcolumn = "yes"
 
 vim.o.updatetime = 250
 
-vim.o.timeoutlen = 1000
+vim.o.timeoutlen = 500
 
 vim.o.splitright = true
 vim.o.splitbelow = true
@@ -154,55 +154,146 @@ require("lazy").setup({
         },
     },
     {
-        "neovim/nvim-lspconfig",
+        'neovim/nvim-lspconfig',
         dependencies = {
-            "stevearc/conform.nvim",
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "hrsh7th/nvim-cmp",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "j-hui/fidget.nvim",
+            { 'mason-org/mason.nvim', opts = {} },
+            'mason-org/mason-lspconfig.nvim',
+            'WhoIsSethDaniel/mason-tool-installer.nvim',
+
+            { 'j-hui/fidget.nvim',    opts = {} },
+
+            'saghen/blink.cmp',
         },
-
         config = function()
-            require("conform").setup({
-                formatters_by_ft = {
-                }
-            })
-            local cmp = require('cmp')
-            local cmp_lsp = require("cmp_nvim_lsp")
-            local capabilities = vim.tbl_deep_extend(
-                "force",
-                {},
-                vim.lsp.protocol.make_client_capabilities(),
-                cmp_lsp.default_capabilities())
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+                callback = function(event)
+                    local map = function(keys, func, desc, mode)
+                        mode = mode or 'n'
+                        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+                    end
 
-            require("fidget").setup({})
-            require("mason").setup()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "gopls",
+                    map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+                    map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+                    map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                    map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+                    map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+                    map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+                    map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+                    map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+                    map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+
+                    -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+                    ---@param client vim.lsp.Client
+                    ---@param method vim.lsp.protocol.Method
+                    ---@param bufnr? integer some lsp support methods only in specific files
+                    ---@return boolean
+                    local function client_supports_method(client, method, bufnr)
+                        if vim.fn.has 'nvim-0.11' == 1 then
+                            return client:supports_method(method, bufnr)
+                        else
+                            return client.supports_method(method, { bufnr = bufnr })
+                        end
+                    end
+
+                    local client = vim.lsp.get_client_by_id(event.data.client_id)
+                    if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+                        local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight',
+                            { clear = false })
+                        vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                            buffer = event.buf,
+                            group = highlight_augroup,
+                            callback = vim.lsp.buf.document_highlight,
+                        })
+
+                        vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                            buffer = event.buf,
+                            group = highlight_augroup,
+                            callback = vim.lsp.buf.clear_references,
+                        })
+
+                        vim.api.nvim_create_autocmd('LspDetach', {
+                            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+                            callback = function(event2)
+                                vim.lsp.buf.clear_references()
+                                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+                            end,
+                        })
+                    end
+
+                    if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+                        map('<leader>th', function()
+                            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+                        end, '[T]oggle Inlay [H]ints')
+                    end
+                end,
+            })
+
+            vim.diagnostic.config {
+                severity_sort = true,
+                float = { border = 'rounded', source = 'if_many' },
+                underline = { severity = vim.diagnostic.severity.ERROR },
+                signs = vim.g.have_nerd_font and {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = '󰅚 ',
+                        [vim.diagnostic.severity.WARN] = '󰀪 ',
+                        [vim.diagnostic.severity.INFO] = '󰋽 ',
+                        [vim.diagnostic.severity.HINT] = '󰌶 ',
+                    },
+                } or {},
+                virtual_text = {
+                    source = 'if_many',
+                    spacing = 2,
+                    format = function(diagnostic)
+                        local diagnostic_message = {
+                            [vim.diagnostic.severity.ERROR] = diagnostic.message,
+                            [vim.diagnostic.severity.WARN] = diagnostic.message,
+                            [vim.diagnostic.severity.INFO] = diagnostic.message,
+                            [vim.diagnostic.severity.HINT] = diagnostic.message,
+                        }
+                        return diagnostic_message[diagnostic.severity]
+                    end,
                 },
+            }
+
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+            local ensure_installed = vim.tbl_keys({})
+            vim.list_extend(ensure_installed, {
+                'stylua',
+            })
+            require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+            require('mason-lspconfig').setup {
+                ensure_installed = { "lua_ls", "gopls", "ts_ls" },
                 automatic_enable = {
-                    exclude = { "lua_ls" }
+                    exclude = {
+                        "lua_ls"
+                    }
                 },
                 handlers = {
-                    function(server_name) -- default handler (optional)
-                        require("lspconfig")[server_name].setup {
-                            capabilities = capabilities
-                        }
+                    function(server_name)
+                        local server = {}
+                        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+                        require('lspconfig')[server_name].setup(server)
                     end,
-                }
-            })
+                },
+            }
 
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup {
+            require('lspconfig').lua_ls.setup({
+                on_attach = function(client, bufnr)
+                    client.notify("workspace/didChangeConfiguration", {
+                        settings = client.config.settings,
+                    })
+
+                    client.config.settings.Lua = vim.tbl_deep_extend(
+                        'force',
+                        client.config.settings.Lua,
+                        {}
+                    )
+
+                    print("Lua version:", client.config.settings.Lua.runtime.version)
+                end,
                 capabilities = capabilities,
                 root_dir = function(fname)
                     local util = require("lspconfig.util")
@@ -217,13 +308,15 @@ require("lazy").setup({
                         return git_root
                     end
 
-                    return util.path.dirname(fname)
+                    return vim.fs.dirname(fname)
                 end,
                 settings = {
                     Lua = {
+                        telemetry = {
+                            enable = false
+                        },
                         runtime = {
                             version = "Lua 5.4",
-                            path = vim.split(package.path, ";"),
                             nonstandardSymbol = {
                                 "/**/",
                                 "`",
@@ -259,67 +352,37 @@ require("lazy").setup({
                                 indent_size = "4",
                             }
                         },
-                    }
-                }
-            }
-
-            local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require('luasnip').lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                }, {
-                    { name = 'buffer' },
-                })
-            })
-
-            vim.diagnostic.config({
-                severity_sort = true,
-                float = { border = "rounded", source = "if_many" },
-                underline = { severity = vim.diagnostic.severity.ERROR },
-                signs = vim.g.have_nerd_font and {
-                    text = {
-                        [vim.diagnostic.severity.ERROR] = "󰅚 ",
-                        [vim.diagnostic.severity.WARN] = "󰀪 ",
-                        [vim.diagnostic.severity.INFO] = "󰋽 ",
-                        [vim.diagnostic.severity.HINT] = "󰌶 ",
                     },
-                } or {},
-                virtual_text = {
-                    source = "if_many",
-                    spacing = 2,
-                    format = function(diagnostic)
-                        local diagnostic_message = {
-                            [vim.diagnostic.severity.ERROR] = diagnostic.message,
-                            [vim.diagnostic.severity.WARN] = diagnostic.message,
-                            [vim.diagnostic.severity.INFO] = diagnostic.message,
-                            [vim.diagnostic.severity.HINT] = diagnostic.message,
-                        }
-                        return diagnostic_message[diagnostic.severity]
-                    end,
                 },
+                on_init = function(client)
+                    local join = vim.fs.joinpath
+                    local path = client.workspace_folders[1].name
+
+                    --[[
+                    if vim.uv.fs_stat(join(path, '.luarc.json'))
+                        or vim.uv.fs_stat(join(path, '.luarc.jsonc'))
+                    then
+                        return
+                    end
+                    ]]
+
+                    client.config.settings.Lua = vim.tbl_deep_extend(
+                        'force',
+                        client.config.settings.Lua,
+                        {}
+                    )
+                end,
             })
-        end
+        end,
     },
-    { -- Autoformat
+
+    {
         "stevearc/conform.nvim",
         event = { "BufWritePre" },
         cmd = { "ConformInfo" },
         keys = {
             {
-                "<leader>f",
+                "<leader>i",
                 function()
                     require("conform").format({ async = true, lsp_format = "fallback" })
                 end,
