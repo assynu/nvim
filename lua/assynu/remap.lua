50ae8b9afe9cd64cdbbbc3ceed46105bdf6d2b98
Dawid <66019090+assynu@users.noreply.github.com>
2025-10-05 12:32:22 +0200

    refactor: save file bind

--- lua/assynu/remap.lua
+++ lua/assynu/remap.lua
@@ -1,18 +1,14 @@
 vim.g.mapleader = " " vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.g.mapleader = " " vim.keymap.set("n", "<leader>w", vim.cmd.w)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.api.nvim_set_keymap("n", "<leader>tf", "<Plug>PlenaryTestFile", { noremap = false, silent = false })
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "=ap", "ma=ap'a")
-
-vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
