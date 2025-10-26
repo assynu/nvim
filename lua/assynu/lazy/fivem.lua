76dd3c58267b5563fa26a31255a864c8be53e627
Dawid <66019090+assynu@users.noreply.github.com>
2025-10-26 10:46:18 +0100

    fix: fivem plugin

--- lua/assynu/lazy/fivem.lua
+++ lua/assynu/lazy/fivem.lua
@@ -1,19 +1,23 @@
 return {
-	dir = "assynu/fivem.nvim",
-	dependencies = {
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	event = "BufReadPre *.lua",
-	opts = {
-		globals = {
-			"lib",
-			"cache",
-			"Core",
-			"MySQL",
-		},
-	},
-	config = function(_, opts)
-		require("fivem-nvim").setup(opts)
-	end,
+    "assynu/fivem.nvim",
+
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "hrsh7th/cmp-nvim-lsp",
+    },
+
+    event = "BufReadPre *.lua",
+
+    opts = {
+        globals = {
+            "lib",
+            "cache",
+            "Core",
+            "MySQL",
+        },
+    },
+
+    config = function(_, opts)
+        require("fivem-nvim").setup(opts)
+    end,
 }
