9c3f71399c0a19b2cce2c4b2ae24262062ba71bb
Dawid <66019090+assynu@users.noreply.github.com>
2025-10-26 10:27:38 +0100

    refactor: move fivem configuration to separate plugin

---
+++ lua/assynu/lazy/fivem.lua
@@ -0,0 +1,19 @@
+return {
+	dir = "assynu/fivem.nvim",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	event = "BufReadPre *.lua",
+	opts = {
+		globals = {
+			"lib",
+			"cache",
+			"Core",
+			"MySQL",
+		},
+	},
+	config = function(_, opts)
+		require("fivem-nvim").setup(opts)
+	end,
+}
