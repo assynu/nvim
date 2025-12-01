8107cafb9308feeee14ab933dfa95013e0ee3fd1
Dawid <66019090+assynu@users.noreply.github.com>
2025-12-01 17:41:02 +0100

    feat: telescope sorting

--- lua/assynu/lazy/telescope.lua
+++ lua/assynu/lazy/telescope.lua
@@ -14,16 +14,42 @@ return {
 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
 	config = function()
-		require("telescope").setup({
+		local telescope = require("telescope")
+		local sorters = require("telescope.sorters")
+		local themes = require("telescope.themes")
+
+		telescope.setup({
+			defaults = {
+				sorting_strategy = "ascending",
+
+				sort_lastused = false,
+				file_sorter = sorters.get_fzy_sorter,
+				generic_sorter = sorters.get_generic_fuzzy_sorter,
+			},
+
+			pickers = {
+				find_files = {
+					sorting_strategy = "ascending",
+					sort_lastused = false,
+					file_sorter = sorters.get_fzy_sorter,
+				},
+				live_grep = {
+					sorting_strategy = "ascending",
+					sort_lastused = false,
+					generic_sorter = sorters.get_generic_fuzzy_sorter,
+				},
+			},
+
 			extensions = {
 				["ui-select"] = {
-					require("telescope.themes").get_dropdown(),
+					themes.get_dropdown(),
 				},
 			},
 		})
-		pcall(require("telescope").load_extension, "fzf")
-		pcall(require("telescope").load_extension, "ui-select")
-		pcall(require("telescope").load_extension, "lazygit")
+
+		pcall(telescope.load_extension, "fzf")
+		pcall(telescope.load_extension, "ui-select")
+		pcall(telescope.load_extension, "lazygit")
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
@@ -32,71 +58,8 @@ return {
 		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
-		vim.keymap.set("n", "<leader>fdg", function()
-			if vim.fn.executable("fd") ~= 1 then
-				vim.notify("fd not found. Install fd for <leader>fdg to work.", vim.log.levels.ERROR)
-				return
-			end
-
-			vim.ui.input({ prompt = "Directory: " }, function(dir_input)
-				if not dir_input or dir_input == "" then
-					return
-				end
-
-				local cwd = vim.fn.getcwd()
-				local pattern = vim.fn.shellescape("*" .. dir_input .. "*")
-				local out = vim.fn.systemlist("fd -t d -H -I -g " .. pattern .. " .")
-				local results = {}
-
-				for _, p in ipairs(out) do
-					local abs = vim.fn.fnamemodify(cwd .. "/" .. p, ":p")
-					if vim.fn.isdirectory(abs) == 1 then
-						table.insert(results, abs)
-					end
-				end
-
-				if #results == 0 then
-					vim.notify("No directories found", vim.log.levels.WARN)
-					return
-				end
-
-				if #results == 1 then
-					require("telescope.builtin").live_grep({
-						search_dirs = { results[1] },
-						prompt_title = "Grep in " .. vim.fn.fnamemodify(results[1], ":t"),
-					})
-					return
-				end
-
-				local pickers = require("telescope.pickers")
-				local finders = require("telescope.finders")
-				local sorters = require("telescope.sorters")
-				local actions = require("telescope.actions")
-				local action_state = require("telescope.actions.state")
-
-				pickers
-					.new({}, {
-						prompt_title = "Choose directory",
-						finder = finders.new_table({ results = results }),
-						sorter = sorters.get_fuzzy_file(),
-						attach_mappings = function(prompt_bufnr)
-							actions.select_default:replace(function()
-								local selection = action_state.get_selected_entry()[1]
-								actions.close(prompt_bufnr)
-								require("telescope.builtin").live_grep({
-									search_dirs = { selection },
-									prompt_title = "Grep in " .. vim.fn.fnamemodify(selection, ":t"),
-								})
-							end)
-							return true
-						end,
-					})
-					:find()
-			end)
-		end)
-
 		vim.keymap.set("n", "<leader>/", function()
-			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+			builtin.current_buffer_fuzzy_find(themes.get_dropdown({
 				winblend = 10,
 				previewer = false,
 			}))
