b130fd3f3379c4965442ab0e210ad965055f3f55
Dawid <66019090+assynu@users.noreply.github.com>
2025-12-15 15:35:45 +0100

    fix: bilnk and supermaven keybind overlap

--- lua/assynu/lazy/blink.lua
+++ lua/assynu/lazy/blink.lua
@@ -3,34 +3,35 @@ return {
 	dependencies = { "rafamadriz/friendly-snippets" },
 	version = "1.*",
 
-	---@module 'blink.cmp'
 	opts = {
 		keymap = {
 			preset = "default",
-			["<Tab>"] = {
-				function(cmp)
-					if vim.b[vim.api.nvim_get_current_buf()].nes_state then
-						cmp.hide()
-						return (require("copilot-lsp.nes").apply_pending_nes() and require("copilot-lsp.nes").walk_cursor_end_edit())
-					end
-					if cmp.snippet_active() then
-						return cmp.accept()
-					else
-						return cmp.select_and_accept()
-					end
-				end,
-				"snippet_forward",
-				"fallback",
-			},
+
+			["<Tab>"] = nil,
+			["<S-Tab>"] = nil,
+
+			["<C-y>"] = { "accept", "fallback" },
+
+			["<C-n>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback" },
 		},
+
 		appearance = {
 			nerd_font_variant = "mono",
 		},
-		completion = { documentation = { auto_show = false } },
+
+		completion = {
+			documentation = { auto_show = false },
+		},
+
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
-		fuzzy = { implementation = "prefer_rust_with_warning" },
+
+		fuzzy = {
+			implementation = "prefer_rust_with_warning",
+		},
 	},
+
 	opts_extend = { "sources.default" },
 }
--- lua/assynu/lazy/supermaven.lua
+++ lua/assynu/lazy/supermaven.lua
@@ -1,8 +1,12 @@
 return {
-	{
-		"supermaven-inc/supermaven-nvim",
-		config = function()
-			require("supermaven-nvim").setup({})
-		end,
-	},
+	"supermaven-inc/supermaven-nvim",
+	config = function()
+		require("supermaven-nvim").setup({
+			keymaps = {
+				accept_suggestion = "<Tab>",
+				clear_suggestion = "<C-]>",
+				accept_word = "<C-l>",
+			},
+		})
+	end,
 }
