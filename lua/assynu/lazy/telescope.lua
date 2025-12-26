75f1b946f9de512b66bb239f71436270ccf3585d
Dawid <66019090+assynu@users.noreply.github.com>
2025-12-26 19:14:40 +0100

    fix: telescope results sorting

--- lua/assynu/lazy/telescope.lua
+++ lua/assynu/lazy/telescope.lua
@@ -3,6 +3,7 @@ return {
 	event = "VimEnter",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "make",
@@ -10,63 +11,90 @@ return {
 				return vim.fn.executable("make") == 1
 			end,
 		},
+
 		{ "nvim-telescope/telescope-ui-select.nvim" },
 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
+
 	config = function()
 		local telescope = require("telescope")
-		local sorters = require("telescope.sorters")
+		local builtin = require("telescope.builtin")
 		local themes = require("telescope.themes")
+		local sorters = require("telescope.sorters")
 
 		telescope.setup({
 			defaults = {
 				sorting_strategy = "ascending",
-
+				case_mode = "ignore_case",
 				sort_lastused = false,
-				file_sorter = sorters.get_fzy_sorter,
-				generic_sorter = sorters.get_generic_fuzzy_sorter,
+				path_display = { "truncate" },
 			},
 
 			pickers = {
 				find_files = {
-					sorting_strategy = "ascending",
-					sort_lastused = false,
-					file_sorter = sorters.get_fzy_sorter,
+					hidden = true,
+
+					find_command = {
+						"fd",
+						"--type",
+						"f",
+						"--strip-cwd-prefix",
+						"--hidden",
+						"--follow",
+					},
+
+					sorter = sorters.empty(),
 				},
+
 				live_grep = {
-					sorting_strategy = "ascending",
+					additional_args = function()
+						return { "--sort", "path" }
+					end,
+
+					sorter = sorters.empty(),
+				},
+
+				buffers = {
 					sort_lastused = false,
-					generic_sorter = sorters.get_generic_fuzzy_sorter,
+					ignore_current_buffer = true,
 				},
 			},
 
 			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = false,
+					override_file_sorter = false,
+					case_mode = "ignore_case",
+				},
+
 				["ui-select"] = {
 					themes.get_dropdown(),
 				},
 			},
 		})
 
-		pcall(telescope.load_extension, "fzf")
-		pcall(telescope.load_extension, "ui-select")
-		pcall(telescope.load_extension, "lazygit")
+		telescope.load_extension("fzf")
+		telescope.load_extension("ui-select")
 
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files (path order)" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep (path order)" })
+		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Grep word" })
+		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics" })
+		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Buffers" })
 
 		vim.keymap.set("n", "<leader>/", function()
-			builtin.current_buffer_fuzzy_find(themes.get_dropdown({
-				winblend = 10,
-				previewer = false,
-			}))
-		end, { desc = "[/] Fuzzily search in current buffer" })
+			builtin.current_buffer_fuzzy_find(
+				themes.get_dropdown({
+					previewer = false,
+					winblend = 10,
+				})
+			)
+		end, { desc = "Fuzzy search buffer" })
 
 		vim.keymap.set("n", "<leader>sn", function()
 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[S]earch [N]eovim files" })
+		end, { desc = "Search Neovim config" })
 	end,
 }
+
