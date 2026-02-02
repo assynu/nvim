487d512f62c51514c02bbef7c612b03eadcd0de2
Dawid <66019090+assynu@users.noreply.github.com>
2026-02-02 17:02:19 +0100

    chore: general config update

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -3,8 +3,14 @@ vim.g.maplocalleader = "\\"
 
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.tabstop = 4
 vim.o.swapfile = false
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+vim.opt.smartindent = true
 
 vim.schedule(function()
 	vim.o.clipboard = "unnamedplus"
