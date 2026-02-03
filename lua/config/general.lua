23a4916372294f1e395f29f9a994d6cfcfd47407
Dawid <66019090+assynu@users.noreply.github.com>
2026-02-03 15:05:47 +0100

    chore: update base configuration

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -1,9 +1,7 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.opt.guicursor = ""
 
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.swapfile = false
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -11,6 +9,23 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 
 vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.undodir = os.getenv("HOME") .. "/.neovim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 200
+vim.opt.colorcolumn = "150"
 
 vim.schedule(function()
 	vim.o.clipboard = "unnamedplus"
@@ -23,5 +38,4 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
-vim.lsp.enable({"lua_ls"})
-vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ 0 }), { 0 })
+vim.lsp.inlay_hint.enable(true, { 0 })
