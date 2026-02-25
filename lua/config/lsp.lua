dbac9aa9017e8d1e75d82c75a19b6a6cac2b8e14
Dawid <66019090+assynu@users.noreply.github.com>
2026-02-25 11:11:42 +0100

    feat: add vue support

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -14,3 +14,19 @@ vim.diagnostic.config({
 		prefix = "",
 	},
 })
+
+local vue_ls_path = vim.fn.expand("$MASON/packages/vue-language-server")
+local vue_plugin_path = vue_ls_path .. "/node_modules/@vue/language-server"
+
+vim.lsp.config("ts_ls", {
+	init_options = {
+		plugins = {
+			{
+				name = "@vue/typescript-plugin",
+				location = vue_plugin_path,
+				languages = { "vue" },
+			},
+		},
+	},
+	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" }
+})
